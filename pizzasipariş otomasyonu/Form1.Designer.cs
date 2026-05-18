eac72567a7b6a3c47b9d9abbf3645838d9341bed
Monster <Monster@DESKTOP-LHVETO7>
2026-05-18 23:02:05 +0300

    Proje dosyası ekle.

---
+++ pizzasipariş otomasyonu/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace pizzasipariş_otomasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.btnMusteriKaydet = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lbladres = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnsiptam = new System.Windows.Forms.Button();
+            this.lblpizsec = new System.Windows.Forms.Label();
+            this.lbladet = new System.Windows.Forms.Label();
+            this.btnSiparisTamamla = new System.Windows.Forms.Label();
+            this.cmbPizza = new System.Windows.Forms.ComboBox();
+            this.lblmussec = new System.Windows.Forms.Label();
+            this.lvSiparis = new System.Windows.Forms.ListView();
+            this.btnSiparisEkle = new System.Windows.Forms.Button();
+            this.nudAdet = new System.Windows.Forms.NumericUpDown();
+            this.cmbMusteri = new System.Windows.Forms.ComboBox();
+            this.dgvSiparisler = new System.Windows.Forms.DataGridView();
+            this.Siparişler = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSiparisler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(224, 151);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(162, 26);
+            this.txtAdSoyad.TabIndex = 0;
+            // 
+            // btnMusteriKaydet
+            // 
+            this.btnMusteriKaydet.Location = new System.Drawing.Point(235, 422);
+            this.btnMusteriKaydet.Name = "btnMusteriKaydet";
+            this.btnMusteriKaydet.Size = new System.Drawing.Size(135, 38);
+            this.btnMusteriKaydet.TabIndex = 1;
+            this.btnMusteriKaydet.Text = "Müşteri Kaydet";
+            this.btnMusteriKaydet.UseVisualStyleBackColor = true;
+            this.btnMusteriKaydet.Click += new System.EventHandler(this.btnMusteriKaydet_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(224, 322);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(162, 26);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(224, 265);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(162, 26);
+            this.txtAdres.TabIndex = 3;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(224, 208);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(162, 26);
+            this.txtTelefon.TabIndex = 4;
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(125, 151);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(93, 20);
+            this.lblAdSoyad.TabIndex = 5;
+            this.lblAdSoyad.Text = "AD SOYAD";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(142, 329);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(58, 20);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "EMAİL";
+            // 
+            // lbladres
+            // 
+            this.lbladres.AutoSize = true;
+            this.lbladres.Location = new System.Drawing.Point(138, 272);
+            this.lbladres.Name = "lbladres";
+            this.lbladres.Size = new System.Drawing.Size(66, 20);
+            this.lbladres.TabIndex = 7;
+            this.lbladres.Text = "ADRES";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(130, 209);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(82, 20);
+            this.lblTelefon.TabIndex = 8;
+            this.lblTelefon.Text = "TELEFON";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = global::pizzasipariş_otomasyonu.Properties.Resources.pizza;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(462, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(179, 174);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnsiptam
+            // 
+            this.btnsiptam.BackColor = System.Drawing.Color.Red;
+            this.btnsiptam.ForeColor = System.Drawing.Color.White;
+            this.btnsiptam.Location = new System.Drawing.Point(1328, 329);
+            this.btnsiptam.Name = "btnsiptam";
+            this.btnsiptam.Size = new System.Drawing.Size(153, 39);
+            this.btnsiptam.TabIndex = 19;
+            this.btnsiptam.Text = "Siparişi tamamla";
+            this.btnsiptam.UseVisualStyleBackColor = false;
+            this.btnsiptam.Click += new System.EventHandler(this.btnsiptam_Click);
+            // 
+            // lblpizsec
+            // 
+            this.lblpizsec.AutoSize = true;
+            this.lblpizsec.Location = new System.Drawing.Point(857, 221);
+            this.lblpizsec.Name = "lblpizsec";
+            this.lblpizsec.Size = new System.Drawing.Size(115, 20);
+            this.lblpizsec.TabIndex = 18;
+            this.lblpizsec.Text = "PİZZA SEÇİMİ";
+            // 
+            // lbladet
+            // 
+            this.lbladet.AutoSize = true;
+            this.lbladet.Location = new System.Drawing.Point(888, 268);
+            this.lbladet.Name = "lbladet";
+            this.lbladet.Size = new System.Drawing.Size(52, 20);
+            this.lbladet.TabIndex = 17;
+            this.lbladet.Text = "ADET";
+            // 
+            // btnSiparisTamamla
+            // 
+            this.btnSiparisTamamla.AutoSize = true;
+            this.btnSiparisTamamla.Location = new System.Drawing.Point(1311, 163);
+            this.btnSiparisTamamla.Name = "btnSiparisTamamla";
+            this.btnSiparisTamamla.Size = new System.Drawing.Size(192, 20);
+            this.btnSiparisTamamla.TabIndex = 16;
+            this.btnSiparisTamamla.Text = "Eklenen Siparişleri Göster";
+            // 
+            // cmbPizza
+            // 
+            this.cmbPizza.FormattingEnabled = true;
+            this.cmbPizza.Location = new System.Drawing.Point(1013, 221);
+            this.cmbPizza.Name = "cmbPizza";
+            this.cmbPizza.Size = new System.Drawing.Size(154, 28);
+            this.cmbPizza.TabIndex = 15;
+            // 
+            // lblmussec
+            // 
+            this.lblmussec.AutoSize = true;
+            this.lblmussec.Location = new System.Drawing.Point(843, 171);
+            this.lblmussec.Name = "lblmussec";
+            this.lblmussec.Size = new System.Drawing.Size(142, 20);
+            this.lblmussec.TabIndex = 14;
+            this.lblmussec.Text = "MÜŞTERİ SEÇİMİ";
+            // 
+            // lvSiparis
+            // 
+            this.lvSiparis.HideSelection = false;
+            this.lvSiparis.Location = new System.Drawing.Point(1303, 191);
+            this.lvSiparis.Name = "lvSiparis";
+            this.lvSiparis.Size = new System.Drawing.Size(200, 97);
+            this.lvSiparis.TabIndex = 13;
+            this.lvSiparis.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnSiparisEkle
+            // 
+            this.btnSiparisEkle.BackColor = System.Drawing.Color.Red;
+            this.btnSiparisEkle.ForeColor = System.Drawing.Color.White;
+            this.btnSiparisEkle.Location = new System.Drawing.Point(1029, 329);
+            this.btnSiparisEkle.Name = "btnSiparisEkle";
+            this.btnSiparisEkle.Size = new System.Drawing.Size(110, 39);
+            this.btnSiparisEkle.TabIndex = 12;
+            this.btnSiparisEkle.Text = "Sipariş Ekle";
+            this.btnSiparisEkle.UseVisualStyleBackColor = false;
+            this.btnSiparisEkle.Click += new System.EventHandler(this.btnSiparisEkle_Click);
+            // 
+            // nudAdet
+            // 
+            this.nudAdet.Location = new System.Drawing.Point(1013, 268);
+            this.nudAdet.Name = "nudAdet";
+            this.nudAdet.Size = new System.Drawing.Size(154, 26);
+            this.nudAdet.TabIndex = 11;
+            // 
+            // cmbMusteri
+            // 
+            this.cmbMusteri.FormattingEnabled = true;
+            this.cmbMusteri.Location = new System.Drawing.Point(1013, 167);
+            this.cmbMusteri.Name = "cmbMusteri";
+            this.cmbMusteri.Size = new System.Drawing.Size(154, 28);
+            this.cmbMusteri.TabIndex = 10;
+            // 
+            // dgvSiparisler
+            // 
+            this.dgvSiparisler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSiparisler.Location = new System.Drawing.Point(1013, 465);
+            this.dgvSiparisler.Name = "dgvSiparisler";
+            this.dgvSiparisler.RowHeadersWidth = 62;
+            this.dgvSiparisler.RowTemplate.Height = 28;
+            this.dgvSiparisler.Size = new System.Drawing.Size(548, 148);
+            this.dgvSiparisler.TabIndex = 20;
+            // 
+            // Siparişler
+            // 
+            this.Siparişler.AutoSize = true;
+            this.Siparişler.Location = new System.Drawing.Point(1025, 442);
+            this.Siparişler.Name = "Siparişler";
+            this.Siparişler.Size = new System.Drawing.Size(74, 20);
+            this.Siparişler.TabIndex = 21;
+            this.Siparişler.Text = "Siparişler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1827, 852);
+            this.Controls.Add(this.Siparişler);
+            this.Controls.Add(this.dgvSiparisler);
+            this.Controls.Add(this.btnsiptam);
+            this.Controls.Add(this.lblpizsec);
+            this.Controls.Add(this.lbladet);
+            this.Controls.Add(this.btnSiparisTamamla);
+            this.Controls.Add(this.cmbPizza);
+            this.Controls.Add(this.lblmussec);
+            this.Controls.Add(this.lvSiparis);
+            this.Controls.Add(this.btnSiparisEkle);
+            this.Controls.Add(this.nudAdet);
+            this.Controls.Add(this.cmbMusteri);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lbladres);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnMusteriKaydet);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Name = "Form1";
+            this.Text = "pizza sipariş sayfası";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSiparisler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button btnMusteriKaydet;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lbladres;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnsiptam;
+        private System.Windows.Forms.Label lblpizsec;
+        private System.Windows.Forms.Label lbladet;
+        private System.Windows.Forms.Label btnSiparisTamamla;
+        private System.Windows.Forms.ComboBox cmbPizza;
+        private System.Windows.Forms.Label lblmussec;
+        private System.Windows.Forms.ListView lvSiparis;
+        private System.Windows.Forms.Button btnSiparisEkle;
+        private System.Windows.Forms.NumericUpDown nudAdet;
+        private System.Windows.Forms.ComboBox cmbMusteri;
+        private System.Windows.Forms.DataGridView dgvSiparisler;
+        private System.Windows.Forms.Label Siparişler;
+    }
+}
+
---
+++ pizzasipariş otomasyonu/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace pizzasipariş_otomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-LHVETO7\\SQLEXPRESS01;Initial Catalog=pizzadb;Integrated Security=True;");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+            MusterileriYukle();
+            PizzalariYukle();
+            SiparisleriListele();
+        
+        }
+        void MusterileriYukle()
+        {
+            cmbMusteri.Items.Clear();
+            baglanti.Open();
+            SqlCommand cmd = new SqlCommand("SELECT MusteriID, AdSoyad FROM Musteriler", baglanti);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                cmbMusteri.Items.Add(new ComboboxItem(dr["AdSoyad"].ToString(), dr["MusteriID"]));
+            }
+            baglanti.Close();
+        }
+
+        void PizzalariYukle()
+        {
+            cmbPizza.Items.Clear();
+            baglanti.Open();
+            SqlCommand cmd = new SqlCommand("SELECT PizzaID, PizzaAdi FROM Pizzalar", baglanti);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                cmbPizza.Items.Add(new ComboboxItem(dr["PizzaAdi"].ToString(), dr["PizzaID"]));
+            }
+           baglanti.Close();
+        }
+
+        void SiparisleriListele()
+        {
+            baglanti.Open();
+            SqlDataAdapter da = new SqlDataAdapter(@"
+        SELECT S.SiparisID, M.AdSoyad, S.SiparisTarihi, S.ToplamTutar
+        FROM Siparisler S
+        INNER JOIN Musteriler M ON S.MusteriID = M.MusteriID", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvSiparisler.DataSource = dt;
+            baglanti.Close();
+        }
+
+
+        private void btnMusteriKaydet_Click(object sender, EventArgs e)
+        {
+
+        
+            baglanti.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Musteriler (AdSoyad, Telefon, Adres, Email) VALUES (@ad, @tel, @adr, @mail)", baglanti);
+            cmd.Parameters.AddWithValue("@ad", txtAdSoyad.Text);
+            cmd.Parameters.AddWithValue("@tel", txtTelefon.Text);
+            cmd.Parameters.AddWithValue("@adr", txtAdres.Text);
+            cmd.Parameters.AddWithValue("@mail", txtEmail.Text);
+            cmd.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Müşteri kaydedildi.");
+            MusterileriYukle();
+            
+
+            
+        }
+
+        private void btnSiparisEkle_Click(object sender, EventArgs e)
+        {
+        
+            var pizza = (ComboboxItem)cmbPizza.SelectedItem;
+            int adet = (int)nudAdet.Value;
+
+            baglanti.Open();
+            SqlCommand cmd = new SqlCommand("SELECT Fiyat FROM Pizzalar WHERE PizzaID = @id", baglanti);
+            cmd.Parameters.AddWithValue("@id", pizza.Value);
+            decimal fiyat = Convert.ToDecimal(cmd.ExecuteScalar());
+            baglanti.Close();
+
+            decimal tutar = fiyat * adet;
+
+            ListViewItem item = new ListViewItem(new[] {
+        pizza.Text,
+        adet.ToString(),
+        fiyat.ToString("C"),
+        tutar.ToString("C"),
+        pizza.Value.ToString() // saklı pizzaid
+    });
+            lvSiparis.Items.Add(item);
+        }
+
+        private void btnsiptam_Click(object sender, EventArgs e)
+        {
+        
+            decimal toplamTutar = 0;
+            foreach (ListViewItem item in lvSiparis.Items)
+            {
+                toplamTutar += decimal.Parse(item.SubItems[3].Text, System.Globalization.NumberStyles.Currency);
+            }
+
+            var musteri = (ComboboxItem)cmbMusteri.SelectedItem;
+
+            baglanti.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Siparisler (MusteriID, ToplamTutar) OUTPUT INSERTED.SiparisID VALUES (@mid, @tutar)", baglanti);
+            cmd.Parameters.AddWithValue("@mid", musteri.Value);
+            cmd.Parameters.AddWithValue("@tutar", toplamTutar);
+            int siparisID = (int)cmd.ExecuteScalar();
+
+            foreach (ListViewItem item in lvSiparis.Items)
+            {
+                int pizzaID = int.Parse(item.SubItems[4].Text);
+                int adet = int.Parse(item.SubItems[1].Text);
+                decimal tutar = decimal.Parse(item.SubItems[3].Text, System.Globalization.NumberStyles.Currency);
+
+                SqlCommand cmdDetay = new SqlCommand("INSERT INTO SiparisDetaylari (SiparisID, PizzaID, Adet, Tutar) VALUES (@sid, @pid, @adet, @tutar)", baglanti);
+                cmdDetay.Parameters.AddWithValue("@sid", siparisID);
+                cmdDetay.Parameters.AddWithValue("@pid", pizzaID);
+                cmdDetay.Parameters.AddWithValue("@adet", adet);
+                cmdDetay.Parameters.AddWithValue("@tutar", tutar);
+                cmdDetay.ExecuteNonQuery();
+            }
+            baglanti.Close();
+
+            MessageBox.Show("Sipariş başarıyla kaydedildi.");
+            lvSiparis.Items.Clear();
+            SiparisleriListele();
+        }
+        public class ComboboxItem
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+
+            public ComboboxItem(string text, object value)
+            {
+                Text = text;
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+    }
+}
+
+
